847aabd55793cfd5247b3171f257707e9575099f
Angel Caballero Dominguez <56036551+angcabdom@users.noreply.github.com>
2021-11-24 22:59:33 +0100

    Added support to nedb for POST method

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 var express = require('express')
 var bodyParser = require('body-parser')
+var DataStore = require('nedb')
 
 const port = 3000
 var BASE_API_PATH = "/api/v1"
+var DB_FILE_NAME = __dirname + "/contacts.json"
 
-var contacts = [
-    {"name": "peter", "phone": "123456789"},
-    {"name": "john", "phone": "987654321"}
-]
 
 console.log("Starting API server...")
 
 const app = express()
 app.use(bodyParser.json())
+
+var db = new DataStore({
+  filename: DB_FILE_NAME,
+  autoload: true
+});
 
 
 app.get('/', (req, res) => {
@@ -20,16 +23,23 @@ app.get('/', (req, res) => {
 })
 
 app.post(BASE_API_PATH + "/contacts", (req, res) => {
-    console.log(Date() + " - GET /contacts");
-    var contact = req.body;
-    contacts.push(contact);
-    res.sendStatus(201);
+  console.log(Date() + " - POST /contacts");
+    
+  var contact = req.body;
+  db.insert(contact, (err) => {
+      if (err) {
+        console.log(Date() + " - " + err);
+        res.sendStatus(500);
+      }else {
+        res.sendStatus(201);
+      }
+  });
 })
 
 app.get(BASE_API_PATH + "/contacts", (req, res) => {
-    console.log(Date() + " - POST /contacts");
-    res.send(contacts);
-  })
+  console.log(Date() + " - GET /contacts");
+  res.send(contacts);
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
